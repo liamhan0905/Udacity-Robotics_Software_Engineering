b343a3fb92ed214af6c39318c3708aa7bc83d1c3
Liam Han <liamhan0905@gmail.com>
2020-06-28 15:55:53 -0700

    adding new projet

--- project_homeServiceRobot/scripts/test_slam.sh
+++ project_homeServiceRobot/scripts/test_slam.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch " &
+xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 5
 xterm  -e  " roslaunch turtlebot_gazebo gmapping_demo.launch " & 
 sleep 5
