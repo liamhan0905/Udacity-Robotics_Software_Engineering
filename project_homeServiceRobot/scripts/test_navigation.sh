d11119945efdbe75cdcfe1c56cc325aaca7e73b4
Liam Han <liamhan0905@gmail.com>
2020-07-07 02:59:36 -0700

    MOD: up to dp to date code

---
+++ project_homeServiceRobot/scripts/test_navigation.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find map)/worlds/finalWorldEdit.world" &
+sleep 5
+xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(rospack find turtlebot_gazebo)/maps/map.yaml" & 
+sleep 5
+xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
+
--- project_homeServiceRobot/scripts/test_slam.sh
+++ project_homeServiceRobot/scripts/test_slam.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch" &
+xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find map)/worlds/finalWorldEdit.world" &
 sleep 5
 xterm  -e  " roslaunch turtlebot_gazebo gmapping_demo.launch " & 
 sleep 5
